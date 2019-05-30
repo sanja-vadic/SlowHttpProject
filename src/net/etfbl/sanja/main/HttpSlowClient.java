bb2a021a65d2ac5996b3df5b57001081f1c80049
Bojan Jelicic <bojan.jelicicdev@gmail.com>
2019-05-30 22:23:23 +0200

    Finished

--- src/net/etfbl/sanja/main/HttpSlowClient.java
+++ src/net/etfbl/sanja/main/HttpSlowClient.java
@@ -15,28 +15,28 @@ public class HttpSlowClient implements Runnable{
 			Socket s = new Socket(InetAddress.getByName("localhost"), 3333);
 			PrintWriter pw = new PrintWriter(s.getOutputStream());
 			pw.print("GET /SigurnostSafe/SlowServlet HTTP/1.1\r\n");
-			TimeUnit.SECONDS.sleep(10);
+//			TimeUnit.SECONDS.sleep(3);
 			pw.print("Host: localhost:8080\r\n");
-			TimeUnit.SECONDS.sleep(10);
+		//	TimeUnit.SECONDS.sleep(3);
 			pw.print("Accept-Encoding: gzip, deflate, br\r\n");
-			TimeUnit.SECONDS.sleep(10);
+		//	TimeUnit.SECONDS.sleep(3);
 			pw.print("Accept-Language: en-US,en;q=0.9\r\n");
-			//TimeUnit.SECONDS.sleep(10);
+		//	TimeUnit.SECONDS.sleep(3);
 			pw.print("Connection: close\r\n");
-			//TimeUnit.SECONDS.sleep(10);
+		//	TimeUnit.SECONDS.sleep(3);
 			pw.print("\r\n");
 			pw.flush();
 			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			String t;
 			while((t = br.readLine()) != null) {
-				System.out.println(t);
+				//System.out.println(t);
 			} 
 			System.out.println("Connected.");
 			br.close();
 			pw.close();
 			s.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 	}
 
--- src/net/etfbl/sanja/main/Main.java
+++ src/net/etfbl/sanja/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		while(true) {
 			for(int i=0; i<500; ++i) {
 				new Thread(new HttpSlowClient()).start();
-			}
+		}
 			try {
 				TimeUnit.MILLISECONDS.sleep(10);
 			} catch (InterruptedException e) {
